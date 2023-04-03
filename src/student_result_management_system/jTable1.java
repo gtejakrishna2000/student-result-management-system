aebf12216ac9a154bd7e254d99eb5e317216910c
gtejakrishna2000 <85777606+gtejakrishna2000@users.noreply.github.com>
2023-04-03 18:11:15 +0530

    Add files via upload

---
+++ src/student_result_management_system/jTable1.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package student_result_management_system;
+
+import javax.swing.table.TableModel;
+
+/**
+ *
+ * @author preet
+ */
+class jTable1 {
+
+    static void setModel(TableModel resultSetToTableModel) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+    
+}
